1a8bfcb4756267699150a5ce87c107d8eba1adcf
ryo katsuma <katsuma@gmail.com>
2008-10-20 19:32:15 +0000

    changed main class
    git-svn-id: http://tokyo-jogging.googlecode.com/svn/trunk@19 7c02460d-8e55-0410-839d-f166c9d7704e

--- tokyo-jogging/start-jogging.sh
+++ tokyo-jogging/start-jogging.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-java -classpath ./:./bin/:./lib/WiiRemoteJ.jar:./lib/bluecove-2.0.3.jar:./lib/jetty-6.1.11.jar:./lib/jetty-util-6.1.11.jar:./lib/json_simple.jar:./lib/servlet-api-2.5-6.1.11.jar tv.katsuma.jog.TokyoJogging
+#!/bin/
+java -classpath ./:./bin/:./lib/WiiRemoteJ.jar:./lib/bluecove-2.0.3.jar:./lib/jetty-6.1.11.jar:./lib/jetty-util-6.1.11.jar:./lib/json_simple.jar:./lib/servlet-api-2.5-6.1.11.jar tj.TokyoJoggingMain
