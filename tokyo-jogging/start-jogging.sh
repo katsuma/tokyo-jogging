d41e0381e26c44be92d4de7c6d581448f6b8ab65
ryo katsuma <katsuma@gmail.com>
2009-10-04 16:38:26 +0000

    support Nunchuk
    git-svn-id: http://tokyo-jogging.googlecode.com/svn/trunk@24 7c02460d-8e55-0410-839d-f166c9d7704e

--- tokyo-jogging/start-jogging.sh
+++ tokyo-jogging/start-jogging.sh
@@ -1,2 +1,2 @@
 #!/bin/
-java -classpath ./:./bin/:./lib/WiiRemoteJ.jar:./lib/bluecove-2.0.3.jar:./lib/jetty-6.1.11.jar:./lib/jetty-util-6.1.11.jar:./lib/json_simple.jar:./lib/servlet-api-2.5-6.1.11.jar tj.TokyoJoggingMain
+java -d32 -Dbluecove.jsr82.psm_minimum_off=true -classpath ./:./bin/:./lib/WiiRemoteJ.jar:./lib/bluecove-2.1.0.jar:./lib/jetty-6.1.11.jar:./lib/jetty-util-6.1.11.jar:./lib/json_simple.jar:./lib/servlet-api-2.5-6.1.11.jar tj.TokyoJoggingMain
